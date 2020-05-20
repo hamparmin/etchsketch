958e4ea42d23344a725c1ee400d5d41d88b124bb
Armin <armin.hamp@minerva.kgi.edu>
2020-05-20 10:12:40 +0200

    Added drawing features

--- myscript.js
+++ myscript.js
@@ -5,8 +5,37 @@ function makeRows(rows,cols){
     container.style.setProperty('--grid-cols',cols);
     for (let i=0;i<(rows*cols);i++){
         let cell=document.createElement("div");
-        cell.innerText=(i+1);
         container.appendChild(cell).className="grid-item";
     };
 };
-makeRows(16,16);
+function getRandomColor(){
+    return "#"+((1<<24)*Math.random()|0).toString(16);
+}
+function colorItWhite(e){
+    e.target.style.background="black";
+}
+function colorItBlack(e){
+    e.target.style.background=getRandomColor();
+}
+function erase(){
+    const cells=Array.from(document.querySelectorAll(".grid-item"));
+    cells.forEach(g => g.addEventListener("mousedown",colorItWhite));
+}
+function activate(){
+    const cells=Array.from(document.querySelectorAll(".grid-item"));
+    cells.forEach(g => g.addEventListener("mouseover",colorItBlack));
+}
+
+function sizeDraw(){
+    let len=window.prompt("How many pixels do you want per side?");
+    //set up grid and draw
+    makeRows(len,len);
+    activate()
+    // const header=document.querySelector("#header");
+    // const eraser=document.createElement("button");
+    // eraser.textContent="Eraser Mode";
+    // header.appendChild(eraser)
+    // eraser.addEventListener('click',erase);
+}
+document.querySelector('#start').addEventListener('click',sizeDraw);
+
